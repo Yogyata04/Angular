7578a5a53dde3436d826b3b37427b0cb537d6ffe
yogyata04 <yogyatachaubey04@gmail.com>
2026-07-21 00:11:41 +0530

    Getter and setter and Directive for Signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
  
+  // Two Way Binding
   // name =signal('Yogyata');
   // age="12";
-  userData=signal({
-    //name:"Rimmi Chaubey",
-    name:"helloooo",
-    age:25,
-    email:"yogyata25@test.com"
-  })
+//   userData=signal({
+//     name:"Rimmi Chaubey",
+//     age:25,
+//     email:"yogyata25@test.com"
+//   })
 
-  updateUserData(key:string,val:string){
-this.userData.update((item)=>({...item,[key]:val}))
-  }
+//   updateUserData(key:string,val:string){
+// this.userData.update((item)=>({...item,[key]:val}))
+//   }
+
+
+//Getter and Setter for Signal
+// userName=signal("Yogyata Chaubey");
+// userData=signal({
+//   college:"pcacs",
+//   email:"pcacs34@test.com"
+// })
+
+// get uName(){
+//   return this.userName();
+// }
+// set uName(val:string){
+//   this.userName.set(val)
+// }
+
+// get userCollege(){
+//   return this.userData().college;
+// }
+// set userCollege(val){
+//   this.userData.update((item)=>({...item,college:val}))
+// }
+// get userEmail(){
+//   return this.userData().email;
+// }
+// set userEmail(val){
+//   this.userData.update((item)=>({...item,email:val}))
+// }
+
+
+// Directive
+// isLogin=signal(true)
+// isLogin=signal(false)
+// users=signal(["rimmi","riya","shivani","hariom"])
+// pColor="pink"
 }
